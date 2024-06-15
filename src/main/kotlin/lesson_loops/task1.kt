977c6e424b11de1eb759509c6f2db6d7b28cd3f1
Михаил Курочкин <eaq19933@gmail.com>
2024-06-15 23:47:33 +0300

    SB-loops crtd for loop

--- src/main/kotlin/lesson_loops/task1.kt
+++ src/main/kotlin/lesson_loops/task1.kt
@@ -1,10 +1,22 @@
 package lesson_loops
 
 fun main() {
+//    countWithWhile()
+    countWithFor()
+}
+
+fun countWithWhile() {
     var counter = 1
     while(counter <= 2030) {
         println(counter)
         counter++
+    }
+    println("End")
+}
+
+fun countWithFor() {
+    for (i in 1..2030) {
+        println(i)
     }
     println("End")
 }
